8b01590f5d4cf00ac1710bd20b557d41f83dd9b4
Bobby Cottle <Bobby.Cottle@gmail.com>
2021-05-26 23:51:43 -0400

    test coroutine/channel functionality

--- src/main/kotlin/SampleMain.kt
+++ src/main/kotlin/SampleMain.kt
@@ -5,46 +5,49 @@ import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
+var keepGoing = true
 fun main(args: Array<String>) {
     val channel = Channel<SampleChannelMessage>(Channel.UNLIMITED);
     val scope = CoroutineScope(Job())
     scope.launch {
-        SubApp(channel).start()
-    }
-    scope.launch {
         PubApp(channel).start()
     }
-
-//    runBlocking {
-//        launch {
-//            SubApp(channel).start()
-//        }
-//        launch {
-//            PubApp(channel).start()
-//        }
-//    }
+    scope.launch {
+        SubApp(channel).start()
+    }
+    // I want to keep the whole program going, but I don't know the proper way yet
+    while (keepGoing) {}
 }
 
 data class SampleChannelMessage(val type: String, val total: Double, val msg: String?) {
-    val time: String = LocalTime.now().format(DateTimeFormatter.BASIC_ISO_DATE)
+    val time: String = LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE)
 }
 
 class PubApp(_channel: SendChannel<SampleChannelMessage>) {
     private val scope = CoroutineScope(Job())
     private val channel = _channel
     fun start() {
+        var loop = 0
         println("PubApp: getting and publishing events...")
         while (getMessage()) {
+            println("Looped ${++loop}")
         }
-        channel.close()
+        println("Customers tired, closing shop.")
+        keepGoing = false
+//        channel.close()
     }
 
     private fun getMessage(): Boolean {
         print("Please enter a message: ")
         val msg = readLine()!!
+        println("\nYou entered: [$msg]")
+        if (msg == "") return false;
+        val words = msg.split(' ').toMutableList()
+//        val msg = readLine()!!
         println("")
         sendEvent(msg)
         return true
